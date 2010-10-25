659f4cf7ebc7fda18e8ad360bc4964aa7bb8741c
Jake <github@jake.otherinbox.com>
2010-10-25 12:05:21 -0700

    Added layout.

--- test/app/controllers/home_controller.rb
+++ test/app/controllers/home_controller.rb
@@ -3,7 +3,8 @@ class HomeController < ApplicationController
 	skip_before_filter :login_required
 
 	def show
-		render :text => 'you are home.'
+		render :text => 'You are home.',
+			:layout => true
 	end
 
 end
