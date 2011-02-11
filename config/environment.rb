2efdae08a6fb2e05bedadb3839983233d02940c2
Jake <github@jakewendt.com>
2011-02-11 10:53:59 -0800

    Updated rails test env to 2.3.11 and cleaned up environment.rb.

--- config/environment.rb
+++ config/environment.rb
@@ -1,24 +1,23 @@
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.10' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.11' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
 
-	config.gem 'jakewendt-calnet_authenticated'	#,
-#		:lib    => 'calnet_authenticated'
+	config.gem 'jakewendt-calnet_authenticated'
+	config.gem 'jakewendt-html_test'
 
 	config.gem 'ryanb-acts-as-list',
 		:lib    => 'acts_as_list'
 
 	config.plugin_paths = [
-		File.expand_path(File.join(File.dirname(__FILE__),'../..'))#,
-	#	File.expand_path(File.join(File.dirname(__FILE__),'../../..','peter'))
+		File.expand_path(File.join(File.dirname(__FILE__),'../..'))
 	]
-	config.plugins = [ :simply_authorized ]#, :html_test, :html_test_extension]
+	config.plugins = [ :simply_authorized ]
 
 	config.frameworks -= [:active_resource]
 
